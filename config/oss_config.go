b7fa2f2ddf0227f68284558da4341a59041a04fa
TangXuanzhao <xuanzhaotang@gmail.com>
2021-07-24 15:05:54 +0800

    add aliyun config

--- config/oss_config.go
+++ config/oss_config.go
@@ -7,12 +7,17 @@ import (
 )
 
 type OssConfig struct {
-	ConfigSource  string `yaml:"config_source"`
-	OssType       string `yaml:"oss_type"`
+	ConfigSource string `yaml:"config_source"`
+	OssType      string `yaml:"oss_type"` // qiniu\aliyun
+	// 通用配置
+	Region        string `yaml:"region"`
 	Buckname      string `yaml:"buckname"`
+	OssDomainName string `yaml:"oss_domain_name"`
 	OssAccessKey  string `yaml:"oss_access_key"`
 	OssSecretKey  string `yaml:"oss_secret_key"`
-	OssDomainName string `yaml:"oss_domain_name"`
+	// 阿里云
+	RoleArn  string `yaml:"role_arn"`
+	Endpoint string `yaml:"endpoint"`
 }
 
 func (s *OssConfig) checkAndFix() {
