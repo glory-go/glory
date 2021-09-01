ffff264567b0fe338e29fc562f1130999cdabfee
TangXuanzhao <xuanzhaotang@gmail.com>
2021-09-02 01:30:51 +0800

    fix grpc start order error

--- service/grpc_service.go
+++ service/grpc_service.go
@@ -40,12 +40,6 @@ func (gs *GrpcService) RegisterUnaryInterceptor(mw ...grpc.UnaryServerIntercepto
 }
 
 func (gs *GrpcService) Run(ctx context.Context) {
-	gs.grpcServer = grpc.NewServer(
-		grpc.UnaryInterceptor(
-			grpc_middleware.ChainUnaryServer(gs.unaryMWs...),
-		),
-	)
-
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%v", gs.conf.addr.Port))
 	if err != nil {
 		log.Fatalf("failed to listen grpc: %v", err)
@@ -58,6 +52,11 @@ func (gs *GrpcService) Run(ctx context.Context) {
 
 // GetGrpcServer 对用户暴露的接口
 func (gs *GrpcService) GetGrpcServer() *grpc.Server {
+	gs.grpcServer = grpc.NewServer(
+		grpc.UnaryInterceptor(
+			grpc_middleware.ChainUnaryServer(gs.unaryMWs...),
+		),
+	)
 	return gs.grpcServer
 }
 
