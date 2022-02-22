2eefa1ca9467f671765814ca3143d422994cfd46
tangxuanzhao <tangxuanzhao@bytedance.com>
2022-02-22 13:51:53 +0800

    update config center interface

--- config_manager/interface.go
+++ config_manager/interface.go
@@ -2,9 +2,10 @@ package configmanager
 
 type ConfigCenter interface {
 	// LoadConfig 从配置中心获取配置
-	LoadConfig(key string) (string, error)
+	LoadConfig(key, group string) (string, error)
 	// SyncConfig 持续从配置中心获取配置，当配置发生变更时，将会修改value指向的值
-	SyncConfig(key string, value *string) error
+	SyncConfig(key, group string, value *string, cancel <-chan struct{}) error
 }
 
-type ConfigCenterBuilder func(map[string]string) (ConfigCenter, error)
+// ConfigCenterBuilder实现了配置中心的初始化工作，conf文件需以yaml形式提供
+type ConfigCenterBuilder func(conf map[string]string) (ConfigCenter, error)
