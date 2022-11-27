259efd99cbd27a42e6d9a588787405ce9cf0816e
TangXuanzhao <xuanzhaotang@gmail.com>
2022-11-27 15:01:47 +0800

    feat: 修复读取env配置的逻辑

--- config/impl.go
+++ config/impl.go
@@ -25,8 +25,33 @@ func convertConfigFromEnv() {
 	keys := configData.AllKeys()
 	for _, key := range keys {
 		match := placeHolderRegexp.FindStringSubmatch(configData.GetString(key))
+		if len(match) < 3 || match[1] != EnvConfigCenterName {
+			configData.Set(key, configData.Get(key)) // 这行不加的话，没有set过的值都会被忽略掉
+			continue
+		}
+		// 获取配置中心实例
+		envCenter := GetEnvConfigCenter()
+		// 读取参数
+		params := make([]any, 0)
+		if err := jsoniter.UnmarshalFromString(match[2], &params); err != nil {
+			panic(err)
+		}
+		// 从配置中心读取配置
+		data, err := envCenter.Get(params...)
+		if err != nil {
+			panic(err)
+		}
+		configData.Set(key, data)
+	}
+}
+
+// convertConfigFromConfigCenter 从配置中心中读取并更新配置
+func convertConfigFromConfigCenter() {
+	keys := configData.AllKeys()
+	for _, key := range keys {
+		match := placeHolderRegexp.FindStringSubmatch(configData.GetString(key))
 		if len(match) < 3 {
-			configData.Set(key, configData.Get(key)) // 这行不加的话，貌似没有set过的值都会被忽略掉
+			configData.Set(key, configData.Get(key)) // 这行不加的话，没有set过的值都会被忽略掉
 			continue
 		}
 		// 获取配置中心实例
@@ -47,9 +72,6 @@ func convertConfigFromEnv() {
 		configData.Set(key, data)
 	}
 }
-
-// convertConfigFromConfigCenter 从配置中心中读取并更新配置
-func convertConfigFromConfigCenter() {}
 
 func getConfig(key string, raw any) {
 	err := configData.UnmarshalKey(key, raw)
